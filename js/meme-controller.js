8056b5596de250df90e9f420d49a9682af9c6f7a
Mor Lev Ari <morshva@gmail.com>
2020-10-23 05:46:16 +0300

    With the editor

--- js/meme-controller.js
+++ js/meme-controller.js
@@ -1,38 +1,57 @@
 'use strict'
+console.log('Hi-controller');
 
 var gCtx;
 var gCanvas;
 
 function onInit() {
+    console.log('enter init');
     gCanvas = document.querySelector('#canvas');
     gCtx = gCanvas.getContext('2d');
-
     createMeme()
+    drawImgFromlocal()
     renderMeme()
 }
 
 
 
+function showCanvas() { //renderMeme?
+    document.querySelector('main').style.display = 'none';
+}
+
 
 function renderMeme() {
+    // console.log('gMeme' ,gMeme);
     var img = new Image()
-    img.src =  gMeme.selectedImgUrl;    // <-----
+    img.src = gMeme.selectedImgUrl;
     img.onload = () => {
         gCtx.drawImage(img, 0, 0, gCanvas.width, gCanvas.height)
-        drawText('hello', 50, 50)
+        drawText(gMeme.lines[0].txt, gMeme.lines[0].x, gMeme.lines[0].y,)
     }
 }
 
 
+
 function drawText(text, x, y) {
-    gCtx.strokeStyle = 'red'
+    gCtx.strokeStyle = 'blue'
     gCtx.fillStyle = 'white'
     gCtx.lineWidth = '2'
-    gCtx.font = '48px Ariel'
-    gCtx.textAlign = 'start'
-    gCtx.fillText(text, x, y)
+    gCtx.font = gMeme.lines[0].size + 'px ' + 'Ariel'
+    gCtx.textAlign = gMeme.lines[0].align
+    gCtx.fillText(gMeme.lines[0].txt, x, y)
     gCtx.strokeText(text, x, y)
 }
+
+
+
+function drawImgFromlocal() {
+    var img = new Image()
+    img.src = gMeme.selectedImgUrl;
+    img.onload = () => {
+        gCtx.drawImage(img, 0, 0, gCanvas.width, gCanvas.height) //img,x,y,xend,yend
+    }
+}
+
 
 
 
---
+++ js/services/meme-service.js
@@ -0,0 +1,42 @@
+'use strict'
+
+// console.log('Hi');
+
+var gMeme = {}
+
+
+
+function createMeme() { 
+
+    var meme = {
+        selectedImgId: 1,
+        selectedLineIdx: 0,
+        selectedImgUrl:'img/3.jpg',
+        lines: [
+            {
+                x: 250,
+                y: 100,
+                txt: 'Hello',
+                size: 50,
+                align: 'center',
+                color: 'red',
+                stroke: 'white'
+            }
+        ]
+    }
+    gMeme = meme;
+}
+console.log('createMeme', gMeme);
+
+
+
+
+
+function downloadCanvas(elLink) {
+    const data = gCanvas.toDataURL()
+    elLink.href = data
+    elLink.download = 'my-img.jpg'
+}
+
+
+
