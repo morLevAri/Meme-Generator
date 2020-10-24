500d9aa10cf3d9da07e9eb272ae9bffb5b50f193
Mor Lev Ari <morshva@gmail.com>
2020-10-24 13:46:27 +0300

    features before bug fixing

--- js/meme-controller.js
+++ js/meme-controller.js
@@ -1,61 +1,117 @@
 'use strict'
 console.log('Hi-controller');
 
-var gCtx;
 var gCanvas;
 
-function onInit() {
-    console.log('enter init');
+function onInit() {  // מה קורה כשהעמוד נטען
+    // console.log('enter init');
     gCanvas = document.querySelector('#canvas');
     gCtx = gCanvas.getContext('2d');
-    createMeme()
-    drawImgFromlocal()
-    renderMeme()
+    createMeme(1);
+    drawImgFromlocal();
+    renderMeme();
+    renderGallery();
 }
 
-
-
-function showCanvas() { //renderMeme?
+function showCanvas(event) {
+    clearCanvas();
+    createMeme(event);
+    renderMeme();
     document.querySelector('main').style.display = 'none';
 }
 
-
 function renderMeme() {
     // console.log('gMeme' ,gMeme);
-    var img = new Image()
+    var img = new Image();
     img.src = gMeme.selectedImgUrl;
     img.onload = () => {
-        gCtx.drawImage(img, 0, 0, gCanvas.width, gCanvas.height)
-        drawText(gMeme.lines[0].txt, gMeme.lines[0].x, gMeme.lines[0].y,)
+        gCtx.drawImage(img, 0, 0, gCanvas.width, gCanvas.height);
+        
+        //לעשות לולאה!
+
+        drawText(gMeme.lines[0].txt, gMeme.lines[0].x, gMeme.lines[0].y,);
+        drawText(gMeme.lines[1].txt, gMeme.lines[1].x, gMeme.lines[1].y,);
+        // drawText(gMeme.lines[2].txt, gMeme.lines[2].x, gMeme.lines[2].y,)
+        // drawText(gMeme.lines[3].txt, gMeme.lines[3].x, gMeme.lines[3].y,)
+
     }
+    
 }
 
+// --------------------------
+
+function onToggleMenu() {
+    toggleMenu();
+}
+// --------------------------
 
 
-function drawText(text, x, y) {
-    gCtx.strokeStyle = 'blue'
-    gCtx.fillStyle = 'white'
-    gCtx.lineWidth = '2'
-    gCtx.font = gMeme.lines[0].size + 'px ' + 'Ariel'
-    gCtx.textAlign = gMeme.lines[0].align
-    gCtx.fillText(gMeme.lines[0].txt, x, y)
-    gCtx.strokeText(text, x, y)
+function onCloseCanvas() {
+    closeCanvas();
 }
 
+function onSetMemeText(ev, text) {
+    const { value } = ev.target;
+    let i = gMeme.selectedLineIdx;
+    gMeme.lines[i].txt = value;
+    setMemeText(text);
+    // console.log(text);
 
-
-function drawImgFromlocal() {
-    var img = new Image()
-    img.src = gMeme.selectedImgUrl;
-    img.onload = () => {
-        gCtx.drawImage(img, 0, 0, gCanvas.width, gCanvas.height) //img,x,y,xend,yend
-    }
+    renderMeme()
 }
 
+function onChangeArrow() {
+    changeArrow();
+    renderMeme();
+}
+
+function onAddAsentence(){
+    addAsentence();
+    renderMeme();
+
+}
+
+function onDeleteText() {
+    deleteText();
+
+}
+
+function onSetFontSize(size) {
+    setFontSize(size);
+    renderMeme();
+}
+
+function onMoveText(dif) {
+    moveText(dif);
+    renderMeme();
+}
+
+function onAlignText(direction){
+    alignText(direction);
+    renderMeme();
+}
+
+function onFontChange(value){
+    fontChange(value);
+    renderMeme();
+}
+
+function onOutlineChange(value){
+    outlineChange(value);
+    renderMeme();
+}
+
+function onColorChange(value){
+    colorChange(value);
+    renderMeme();
+}
+
+// --------------------------
+
+// function shareMeme(this){}
 
 
 
 
-// function onEditImage(){
 
-// }
+
--- js/services/meme-service.js
+++ js/services/meme-service.js
@@ -3,40 +3,168 @@
 // console.log('Hi');
 
 var gMeme = {}
+var gCtx;
 
-
-
-function createMeme() { 
-
+function createMeme(imgId) {
     var meme = {
-        selectedImgId: 1,
+        selectedImgId: imgId,
         selectedLineIdx: 0,
-        selectedImgUrl:'img/3.jpg',
-        lines: [
-            {
-                x: 250,
-                y: 100,
-                txt: 'Hello',
-                size: 50,
-                align: 'center',
-                color: 'red',
-                stroke: 'white'
-            }
-        ]
+        selectedImgUrl: `img/${imgId}.jpg`,
+        lines:
+            [
+                {
+                    txt: 'Hello',
+                    x: 250,
+                    y: 100,
+                    size: 50,
+                    font: 'impact',
+                    align: 'center',
+                    color: 'white',
+                    stroke: 'black',
+                    lineWidth: 2,
+                },
+                {
+                    txt: 'There',
+                    x: 250,
+                    y: 450,
+                    size: 50,
+                    font: 'impact',
+                    align: 'center',
+                    color: 'white',
+                    stroke: 'black',
+                    lineWidth: 2,
+                },
+
+
+            ]
     }
     gMeme = meme;
+    // console.log('gMeme', gMeme);
+
 }
-// console.log('createMeme', gMeme);
 
+function drawText(text, x, y) {
+    // console.log('gCtx', gCtx);
 
+    gCtx.font = gMeme.lines[0].size + 'px ' + gMeme.lines[0].font;
+    gCtx.textAlign = gMeme.lines[0].align;
+    gCtx.fillStyle = gMeme.lines[0].color;
+    gCtx.strokeStyle = gMeme.lines[0].stroke;
+    gCtx.lineWidth = gMeme.lines[0].lineWidth;
+    gCtx.fillText(text, x, y);      //<---
+    gCtx.strokeText(text, x, y);   //<---
+}
 
+function renderGallery() {
+    var strHTML = '';
+    for (var i = 1; i <= 20; i++) {
+        strHTML += `<div class="gallery-img"onclick="showCanvas(${i})">\n \t<img src="img/${i}.jpg">\t</div>\n`
+    }
+    document.querySelector('.gallery-grid').innerHTML = strHTML;
+}
 
+function drawImgFromlocal() {
+    var img = new Image()
+    img.src = gMeme.selectedImgUrl;
+    img.onload = () => {
+        gCtx.drawImage(img, 0, 0, gCanvas.width, gCanvas.height) //img,x,y,xend,yend
+    }
+}
+
+function toggleMenu() {
+    document.body.classList.toggle('menu-open');
+}
+
+function closeCanvas() {
+    document.querySelector('main').style.display = 'block';
+
+}
+
+function setMemeText(text) {
+    let i = gMeme.selectedLineIdx;
+    gMeme.lines[i].txt = text;
+
+    // var elFocus = document.querySelector("#user-text");
+    // elFocus.classList.add("focus");
+    // console.log('elFocus', );
+
+    // להוסיף מסגרת לטקסט
+}
+
+function changeArrow() {
+
+    if (gMeme.selectedLineIdx === gMeme.lines.length - 1) {
+        gMeme.selectedLineIdx = 0;
+        return;
+    }
+    gMeme.selectedLineIdx++;
+
+}
+
+function createLine(posX, posY) {
+    return { txt: 'puki', x: posX, y: posY, size: 50, font: 'impact', align: 'center', color: 'white', stroke: 'black', lineWidth: 2.5 };
+}
+
+function addAsentence() {
+    console.log('gMeme', gMeme);
+    gMeme.lines.push(createLine(250, 250));
+    gMeme.selectedLineIdx = gMeme.lines.length - 1;
+    // drawText(gMeme.lines[2].txt, gMeme.lines[2].x, gMeme.lines[2].y,)
+}
+
+function deleteText() {
+    clearCanvas();
+    drawImgFromlocal();
+
+}
+
+function clearCanvas() {
+    gCtx.clearRect(0, 0, gCanvas.width, gCanvas.height);
+}
+
+function setFontSize(size) {
+    let i = gMeme.selectedLineIdx;
+    gMeme.lines[i].size = parseInt(size);
+}
+
+function moveText(dif) {
+    let i = gMeme.selectedLineIdx;
+    gMeme.lines[i].y += dif;
+
+}
+
+function alignText(direction) {
+    switch (direction) {
+        case 'left':
+            gMeme.lines[gMeme.selectedLineIdx].align = 'left';
+            gMeme.lines[gMeme.selectedLineIdx].x = 10;
+            break;
+        case 'center':
+            gMeme.lines[gMeme.selectedLineIdx].align = 'center';
+            gMeme.lines[gMeme.selectedLineIdx].x = 250;
+            break;
+        case 'right':
+            gMeme.lines[gMeme.selectedLineIdx].align = 'right';
+            gMeme.lines[gMeme.selectedLineIdx].x = 490;
+            break;
+    }
+
+}
+
+function fontChange(value) {
+    gMeme.lines[gMeme.selectedLineIdx].font = value;
+}
+
+function outlineChange(value) {
+    gMeme.lines[gMeme.selectedLineIdx].stroke = value;
+}
+
+function colorChange(value) {
+    gMeme.lines[gMeme.selectedLineIdx].color = value;
+}
 
 function downloadCanvas(elLink) {
-    const data = gCanvas.toDataURL()
-    elLink.href = data
-    elLink.download = 'my-img.jpg'
+    const data = gCanvas.toDataURL();
+    elLink.href = data;
+    elLink.download = 'my-meme.jpg';
 }
-
-
-
